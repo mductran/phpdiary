3b27e1e89273ce8b54a6d2194f9c8acf93a8cff6
Duc <ductran032118@gmail.com>
2022-09-19 21:51:55 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,13 +1,12 @@
 <?php
-
-	$servername = $_SESSION['ep'];
-	$username = $_SESSION['un'];
-	$password = $_SESSION['pw']
-	$database = $_SESSION['db'];
-
+	$servername = "taskc-db.caqshf3dyoyx.us-east-1.rds.amazonaws.com";
+	$username = "admin";
+	$password = "password";
+	$database = "taskc";
+	
+	// $link = mysqli_connect($servername, $username, $password, $database);
 	$link = mysqli_connect($servername, $username, $password, $database);
 	if(mysqli_connect_error()){
-
 		die("Problem...!!");
 	}
 
